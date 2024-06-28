7607261e79b44be53167ebdb64a4210fdbfb0582
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-27 20:28:14 -0700

    ASSIG4.2:DONE

--- main.py
+++ main.py
@@ -11,14 +11,20 @@ def main():
     ########################################
     """
 
+        
+    #generate sequnce
     for i in range(2,N):
         a3 = result[i-1] + result[i-2]
         result.append(a3)
- 
+        
+        
+    print(result)
+
     ########################################
     # Do not delete the return statement
     ########################################
-    return result
+    
+#return result
 
 
 
